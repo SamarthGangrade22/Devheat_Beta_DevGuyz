177db12206172fc2ba80b31bf57e7407f6d276d4
PrateekJaiswal16 <prateekja.swal16@gmail.com>
2023-10-01 21:55:36 +0530

    Home page

---
+++ index.js
@@ -0,0 +1,7 @@
+const element=document.querySelector(".button");
+document.addEventListener("click",button_publish);
+// var model=document.querySelector(".model").innerHTML
+//const model
+function button_publish(){
+    document.querySelector(".bars").innerHTML='Hello';
+}
