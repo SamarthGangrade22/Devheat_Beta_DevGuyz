38f4a1419248c1766cb23b551d1d90bbab3cb6ad
SamarthGangrade22 <samarthgangrade@gmail.com>
2023-10-06 06:47:33 +0530

    working website

--- index.js
+++
@@ -1,7 +0,0 @@
-// const element=document.querySelector(".button");
-// document.addEventListener("click",button_publish);
-// // var model=document.querySelector(".model").innerHTML
-// //const model
-const offcanvasElementList = document.querySelectorAll('.offcanvas')
-const offcanvasList = [...offcanvasElementList].map(offcanvasEl => new bootstrap.Offcanvas(offcanvasEl))
-
